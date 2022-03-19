813cd8cd3e77ac2b0cdce625b24c5eba075a02c8
Melodies-Tang <1254641909@qq.com>
2022-03-19 19:24:24 +0800

    Refactor code

--- SDK/SDK_python/CodeCraft-2022/src/CodeCraft-2022.py
+++ SDK/SDK_python/CodeCraft-2022/src/CodeCraft-2022.py
@@ -1,109 +1,87 @@
-import csv
-import numpy as np
 import os
-import configparser
-
-N = 0
-site_bandwidth = []
-site_name = []
-data_path = "../../../../data/"
-with open(os.path.abspath(data_path + "site_bandwidth.csv")) as f:
-    reader = csv.reader(f)
-    firstLine = True
-    name = ""
-    bd = 0
-    for row in reader:
-        if firstLine:
-            firstLine = False
-        else:
-            name, bd = row
-            site_name.append(name)
-            site_bandwidth.append(int(bd))
-    N = len(site_name)
-
-client_name = []
-demands = dict()
-with open(os.path.abspath(data_path + "demand.csv")) as f:
-    reader = csv.reader(f)
-    firstLine = True
-    M = 0
-    for row in reader:
-        if firstLine:
-            firstLine = False
-            M = len(row) - 1
-            client_name = row[1:]
-        else:
-            demands[row[0]] = [int(band) for band in row[1:]]
-
-cf = configparser.ConfigParser()
-cf.read(os.path.abspath(data_path + "config.ini"))
-QoS = cf.getint("config", "qos_constraint")
-
-graph = dict()
-with open(os.path.abspath(data_path + "qos.csv")) as f:
-    reader = csv.reader(f)
-    firstLine = True
-    for s, row in enumerate(reader):
-        if firstLine:
-            firstLine = False
-            continue
-        s -= 1
-        graph[s] = set()
-        for c, q in enumerate(row[1:]):
-            if int(q) < QoS:
-                c = c + N  # shift of client id
-                graph[s].add(c - 1 + N)
-                if not graph.__contains__(c):
-                    graph[c] = set()
-                graph[c].add(s)
-
-used = [[0 for t in range(len(demands))] for _ in range(N)]  # server:time:total
-t = 0
-assigns = []
-for time, demand in demands.items():
-    assign = dict()  # client: server:band
-    client_demand = [[demand[c], c + N] for c in range(len(demand))]
-    client_demand.sort(reverse=True)
-    cap = site_bandwidth.copy()
-    for band, c in client_demand:
-        assign[c] = dict()
-        if band == 0:  # zero requirement
-            continue
-        cnt = len(graph[c])
-        while band > 0:
-            for site in graph[c]:
-                avg = band // cnt
-                cur = min(band // cnt, cap[site])
-                cap[site] -= cur
-                used[site][t] += cur
-                band -= cur
-                if not assign[c].__contains__(site):
-                    assign[c][site] = cur
-                else:
-                    assign[c][site] += cur
-                cnt -= 1
-    assigns.append(assign)
-    t += 1
-
-total_cost = 0
-pos = int(len(demands) * 0.95)
-for site in range(N):
-    used[site].sort()
-    total_cost += used[site][pos]
+import input
 
 
-output = []
-for assign in assigns:
-    for client in range(M):
-        cur = client_name[client] + ":"
-        for site, band in assign[client + N].items():
-            cur += "<{},{}>,".format(site_name[site], band)
-        if cur[-1] == ",":
-            cur = cur[:-1]
-        output.append(cur)
+def getDemand(elem):
+    return elem[1]
 
-with open(os.path.abspath("../../output/solution.txt"), 'w') as f:
-    for row in output:
-        f.write(row + "\n")
 
-check = 0
+root_path = "/"
+# root_path = "/home/melodies/HWcom/SDK/SDK_python/"
+data_path = root_path + "data/"
+output_path = root_path + "output/"
+
+if __name__ == '__main__':
+    '''
+    N: number of sites
+    M: number of clients
+    QoS: configuration for connectivity
+    site_bandwidth: [name:band_capacity for name in site_names]
+    demands: [[name:demand_t for name in client_names] for t in times (index)]
+    graph: bilateral connectivity between sites and clients 
+    used: [[band_used_t for t in times] indexed by server name]
+    '''
+    N, site_bandwidth = input.getSiteInfo(data_path)
+    M, demands = input.getClientInfo(data_path)
+    QoS = input.getConfig(data_path)
+    site_client, client_site = input.buildGraph(data_path, list(demands[0].keys()), QoS)
+    site_names = site_bandwidth.keys()
+    client_names = demands[0].keys()
+    times = len(demands)
+
+    used = dict()  # site:time(index):total_usage, for sake of calculating COST
+    for site in site_names:
+        used[site] = [0 for t in range(times)]
+    assigns = []  # time(index):client:site:band_assigned, for solution
+
+    for time, demand in enumerate(demands):
+        # demand form:: client:demand
+        cur_client_demand = list(demand.items())  # each element: (client, band demand)
+        cur_client_demand.sort(key=getDemand, reverse=True)  # sort by band
+        capacity = site_bandwidth.copy()  # remained capacity for current time
+        cur_assign = dict()
+        for client, band in cur_client_demand:
+            client_assign = cur_assign[client] = dict()
+            if band == 0:  # zero requirement
+                continue
+            available_sites = client_site[client]
+            total_cnt = len(available_sites)  # available sites for current client
+            while total_cnt > 0 and band > 0:
+                cnt = total_cnt
+                for site in available_sites:
+                    avg = max(band // cnt, 1)  # rolling average
+                    cur = min(band // cnt, capacity[site])  # real assign
+                    if cur == 0:  # this site cannot provide bandwidth
+                        total_cnt -= 1
+                        cnt -= 1
+                        continue
+                    capacity[site] -= cur
+                    used[site][time] += cur
+                    band -= cur
+
+                    if not client_assign.__contains__(site):
+                        client_assign[site] = cur
+                    else:
+                        client_assign[site] += cur
+                    cnt -= 1
+        assigns.append(cur_assign)
+
+    total_cost = 0
+    pos = int(len(demands) * 0.95)
+    for site, usage in used.items():
+        usage.sort()
+        total_cost += usage[pos]
+
+    output = []
+    for assign in assigns:
+        for client, client_assign in assign.items():
+            cur = client + ":"
+            for site, band in client_assign.items():
+                cur += "<{},{}>,".format(site, band)
+            if cur[-1] == ",":
+                cur = cur[:-1]
+            output.append(cur)
+
+    with open(os.path.abspath(output_path + "solution.txt"), 'w') as f:
+        for row in output:
+            f.write(row + "\n")
